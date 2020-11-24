3669c8bfca8e598821564e52877ff8b441ff69a8
Raghav Medapati <medapati.raghu@gmail.com>
2020-11-24 00:43:44 -0700

    first commit

---
+++ src/main/java/com/raghav/springbootcontractfirstapi/SpringBootContractFirstApiApplication.java
@@ -0,0 +1,13 @@
+package com.raghav.springbootcontractfirstapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootContractFirstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootContractFirstApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raghav/springbootcontractfirstapi/SpringBootContractFirstApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.springbootcontractfirstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContractFirstApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
